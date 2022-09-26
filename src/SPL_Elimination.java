f63514cb82c92e9733fb4bfeabc1efbe5e166a05
rayhanp1402 <103352278+rayhanp1402@users.noreply.github.com>
2022-09-27 02:55:02 +0700

    Add files via upload

---
+++ src/SPL_Elimination.java
@@ -0,0 +1,47 @@
+public class SPL_Elimination {
+    public static double[] gauss_jordan(double[][] A, double[][] b, int m, int n){
+        int i, j;
+        int zero_row = 0;
+        double[] solution = new double[n];
+        double[][] augmented = new double[m][n+1];
+
+        // Set Solutions to 0
+        for(i = 0; i < n; i++){
+            solution[i] = 0d;
+        }
+
+        // Create Matrix Augmented
+        for(i = 0; i < m; i++){
+            for(j = 0; j < n+1; j++){
+                if(j == n){
+                    augmented[i][j] = b[i][0];
+                } else{
+                    augmented[i][j] = A[i][j];
+                }
+            }
+        }
+
+        // Gaussian Elimination
+        Elimination.gauss_jordan(augmented, m, n+1);
+
+        // Check Solution
+        for(j = 0; j < n+1; j++){
+            zero_row = j;
+            if(augmented[m-1][j] != 0){
+                break;
+            }
+        }
+        if(zero_row == n && augmented[m-1][n] == 0){
+            System.out.println("Solusi SPL berbentuk parametrik");
+            return solution;
+        } else if(zero_row == n){
+            System.out.println("SPL tidak memiliki solusi");
+            return solution;
+        } else{
+            for(i = 0; i < m; i++){
+                solution[i] = augmented[i][n];
+            }
+            return solution;
+        }
+    }
+}
