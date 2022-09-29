5beadbf2a74c7e35fa849b05696552ea275900e6
rayhanp1402 <103352278+rayhanp1402@users.noreply.github.com>
2022-09-29 14:20:17 +0700

    Update Interpolation.java

--- src/Interpolation.java
+++ src/Interpolation.java
@@ -1,7 +1,7 @@
 import java.lang.Math;
 
 public class Interpolation {
-    public static double polinom(double[][] A, double[][] b, double x){
+    public static void polinom(double[][] A, double[][] b, double x){
         int i, j;
         int m = A.length, n = A[0].length;
         double y = 0d;
@@ -14,6 +14,22 @@ public class Interpolation {
         for(i = 0; i < n; i++){
             y += solution[i] * Math.pow(x, i);
         }
-        return y;
+        
+        // Output Interpolation Result
+        System.out.print("f(x) = ");
+        System.out.printf("%.4f", solution[n-1]);
+        System.out.print("x^" + (n-1));
+        for(i = n-2; i >= 2; i--){
+            System.out.print(" + ");
+            System.out.printf("%.4f", solution[i]);
+            System.out.print("x^" + i);
+        }
+        System.out.print(" + ");
+        System.out.printf("%.4f", solution[1]);
+        System.out.print("x");
+        System.out.print(" + ");
+        System.out.printf("%.4f,\n", solution[0]);
+        System.out.print("f(" + x + ") = ");
+        System.out.printf("%.4f\n", y);
     }
 }
