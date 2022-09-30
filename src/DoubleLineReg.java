90c57da76f8f2f222a229307ffe53141f56833d2
yansans <66671259+yansans@users.noreply.github.com>
2022-09-30 20:15:12 +0700

    Adding double linear regression

---
+++ src/DoubleLineReg.java
@@ -0,0 +1,20 @@
+import static Tubes.Cofactor.*;
+import static Tubes.MatrixMultiplication.*;
+import static Tubes.Identity.*;
+
+public class DoubleLineReg {
+
+    public static double[][] doubRegSolve(double[][] x, double[][] y){
+        double[][] xt ; //[x.length][x[0].length]
+        double[][] invxxt ;
+        double [][] xxt ;
+        xt = transpose(x);
+        xxt = PerkalianMatrix(xt,x);
+        invxxt = xxt;
+        inverse(invxxt,xxt.length);
+        // b =  (XX^)^-1X^Y
+        double[][] solve = PerkalianMatrix(PerkalianMatrix(invxxt, xt),y);
+        return solve;
+    }
+
+}
