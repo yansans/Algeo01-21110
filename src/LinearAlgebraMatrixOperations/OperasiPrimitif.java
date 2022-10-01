eeb77b20ea6e8db6079ddc513bbbbd3344a8cdfc
yansans <66671259+yansans@users.noreply.github.com>
2022-10-01 11:23:49 +0700

    packaging src to LinearAlgebraMatrixOperations

---
+++ src/LinearAlgebraMatrixOperations/OperasiPrimitif.java
@@ -0,0 +1,53 @@
+package src.LinearAlgebraMatrixOperations;
+
+public class OperasiPrimitif {
+
+    public static void copyMatrix(double[][] matrix, double[][] copy){
+        int i, j;
+
+        for(i = 0; i < matrix.length; i++){
+            for(j = 0; j < matrix[0].length; j++){
+                copy[i][j] = matrix[i][j];
+            }
+        }
+    }
+
+    public static double[][] PerkalianMatrix(double[][] matrix1, double[][] matrix2){
+        int n = matrix1.length;
+        int m = matrix2[0].length;
+        int o = matrix1[0].length;
+        double[][] matrix3 = new double[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int k=0;k<o;k++){
+                    matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return matrix3;
+    }
+
+    public static double[][] PerkalianMatrixKonstanta(double[][] matrix, double konstanta){
+        int n = matrix.length;
+        int m = matrix[0].length;
+        double[][] matrix2 = new double[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix2[i][j] = matrix[i][j] * konstanta;
+            }
+        }
+        return matrix2;
+    }
+
+    public static double[] SolusiSPL(double[][] matrix, double[] nilai){
+        int dimensi = matrix.length, col = matrix[0].length;
+        double[] solusi = new double[col];
+        for(int i=0;i<dimensi;i++){
+            for(int j=0;j<dimensi;j++){
+                solusi[i] += matrix[i][j] * nilai[j];
+            }
+        }
+        return solusi;
+    }
+
+}
